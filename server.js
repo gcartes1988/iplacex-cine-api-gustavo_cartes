ebc9af5a615054e503761f9807119b17df7a8d98
gcartes1988 <156698135+gcartes1988@users.noreply.github.com>
2025-01-27 13:27:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,24 +4,25 @@ import client from './src/common/db.js';
 import peliculaRoutes from './src/pelicula/peliculaRoutes.js'; 
 import actorRoutes from './src/actor/actorRoutes.js';  
 
-const PORTS = 4000;
+const port = process.env.PORT || 4000; // Usa el puerto dinámico de Render o 4000 si no está disponible
 const app = express();
 
-app.use(express.json());  
-app.use(urlencoded({ extended: true }));  
-app.use(cors());  
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(cors());
 
-app.all('/', (req, res) => { return res.status(200).send('Bienvenido al cine Iplacex');});
+app.all('/', (req, res) => {
+  return res.status(200).send('Bienvenido al cine Iplacex');
+});
 
 app.use('/api', peliculaRoutes);
-
-app.use('/api', actorRoutes);  
+app.use('/api', actorRoutes);
 
 await client.connect()
   .then(() => {
     console.log('Conectado al clúster');
-    app.listen(PORTS, () => {
-      console.log(`Servidor corriendo en http://localhost:${PORTS}`);
+    app.listen(port, () => {
+      console.log(`Servidor corriendo en http://localhost:${port}`);
     });
   })
   .catch((e) => {
